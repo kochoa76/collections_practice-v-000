899b252308f22890dcf783714f38bb931cbe41f8
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-05-14 20:42:28 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -40,5 +40,11 @@ def sum_array(array)
 end
 
 def add_s(array)
-array[1,2].each_with_index.collect{|element, index| }
-end 
+array.collect do |word|
+  if array[1] == word
+    word
+  else
+    word + "s"
+  end
+end
+end
